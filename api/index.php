7b0676177f9025ca9d73bfdfc1545fdf8168823b
nabila240180111-prog <nabila.240180111@mhs.unimal.ac.id>
2026-07-20 23:19:13 +0700

    Fix Laravel base path detection on Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,6 @@
 <?php
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+
 // Forward Vercel serverless requests to Laravel's public entrypoint
 require __DIR__ . '/../public/index.php';
